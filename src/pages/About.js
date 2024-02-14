de65353e0276de46dc18274714bbd42687bc68c4
BetaPeche <lolopellegrino@gmail.com>
2024-02-14 17:46:33 +0100

    end first party

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium nisi officiis nobis sunt accusantium ea magni vitae quae quis repellendus! Ratione odio quam, exercitationem incidunt numquam natus? Suscipit, hic amet. Ad totam at non quisquam aliquam animi, deserunt culpa, tempora dolore veritatis facilis assumenda. Quaerat doloremque consequatur consectetur incidunt sed maiores expedita in pariatur at culpa distinctio eveniet provident fugiat illo dicta totam sint est consequuntur, molestias explicabo officia maxime temporibus numquam ut. Ipsum eum accusamus itaque illum voluptates omnis animi deserunt porro eligendi optio dolorem natus temporibus sit, eveniet in asperiores sapiente repellendus nemo ipsa officiis, consequatur est. Nesciunt!</p>
+        </div>
+    );
+};
+
+export default About;
